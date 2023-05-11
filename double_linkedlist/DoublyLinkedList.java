27e9d6b732b4eb2af3e26a33292f897497e4f8cf
andrewmalley725 <andrew.alley99@gmail.com>
2023-05-11 00:16:08 -0500

    set up main constructor for dll

---
+++ double_linkedlist/DoublyLinkedList.java
@@ -0,0 +1,77 @@
+package double_linkedlist;
+
+public class DoublyLinkedList {
+    int length = 0;
+    Node head;
+    Node tail;
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public DoublyLinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length++;
+    }
+
+    private void printItems() {
+        Node temp = head;
+        while(temp.next != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.print(temp.value);
+        System.out.println("");
+    }
+
+    public void summary() {
+        System.out.println("");
+        System.out.println("Head: " + head.value);
+        System.out.println("Tail: " + tail.value);
+        System.out.println("Length: " + length);
+        System.out.println("");
+        this.printItems();
+        System.out.println("");
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (length == 0) {
+            head = newNode;
+            tail = newNode;
+            length++;
+        }
+        else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+            length++;
+        }
+    }
+
+    public Node removeLast() {
+        if (length == 0) return null;
+
+        if (length == 1) {
+            Node temp = head;
+            head = null;
+            tail = null;
+            length--;
+            return temp;
+        }
+
+        Node temp = tail;
+        tail = temp.prev;
+        temp.prev = null;
+        tail.next = null;
+        length--;
+        return temp;
+    }
+}
--- double_linkedlist/Main.java
+++ double_linkedlist/Main.java
@@ -1,5 +1,10 @@
 package double_linkedlist;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        DoublyLinkedList myList = new DoublyLinkedList(0);
+        myList.append(1);
+        myList.append(2);
+        myList.summary();
+    }
 }
