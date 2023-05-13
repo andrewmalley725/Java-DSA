47f77b8207e78eabb519a50c5426b7140552be63
andrewmalley725 <andrew.alley99@gmail.com>
2023-05-13 12:07:21 -0500

    graphs

--- graph/Main.java
+++ graph/Main.java
@@ -2,6 +2,12 @@ package graph;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Graph myGraph = new Graph();
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        myGraph.addVertex("C");
+        myGraph.addEdge("A", "B");
+        myGraph.addEdge("B", "C");
+        myGraph.printMap();
     }
 }
